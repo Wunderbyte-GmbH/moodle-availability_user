509c9ea7e17e8a3f0eaf58c21b57987e54bce1f8
cbadusch <hello@christianbadusch.codes>
2026-02-25 14:59:12 +0100

    GL-69 Kontrolle availability_user bei der Auswahl vieler Benutzer Wunderbyte-GmbH/wko-complete#6

--- classes/condition.php
+++ classes/condition.php
@@ -41,7 +41,7 @@ class condition extends \core_availability\condition {
     public function __construct($structure) {
         $this->userids = [];
         if (isset($structure->userids)) {
-            $this->userids = $structure->userids;
+            $this->userids = (array)$structure->userids;
         }
         // Ensure compatibility with old version.
         if (isset($structure->userid)) {
